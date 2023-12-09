7f93880edbd35eb6c4d3272f714c574002b68495
JoeyThePianist <24552347+JoeyThePianist@users.noreply.github.com>
2023-12-08 17:27:59 -0700

    Initial commit, with day 1 complete

---
+++ app.py
@@ -0,0 +1,21 @@
+age = 34
+jan_days = 31
+feb_days = 28
+mar_days = 31
+apr_days = 30
+may_days = 31
+jun_days = 30
+jul_days = 31
+aug_days = 31
+sep_days = 30
+oct_days = 31
+nov_days = 30
+dec_days = 31
+days_per_year = jan_days + feb_days + mar_days + apr_days + may_days + jun_days + jul_days + aug_days + sep_days + oct_days + nov_days + dec_days
+weeks_per_year = 52
+months_per_year = 12
+r = 5
+area = 3.141 * (r*r)
+print(f"There are {days_per_year * 27} days, {weeks_per_year * 27} weeks, or {months_per_year * 27} months in 27 years.")
+print(f"I am {age} years old.")
+print(f"The area of a circle with a radius of 5 units is {area}.")
