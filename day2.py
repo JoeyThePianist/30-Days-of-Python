8477e2ece98179d844c64cfda6335ad65beb38d0
JoeyThePianist <24552347+JoeyThePianist@users.noreply.github.com>
2023-12-08 18:08:35 -0700

    Day 2 with exercise requirements and answers.

---
+++ day2.py
@@ -0,0 +1,7 @@
+#1. Ask the user for their name and age, assign theses values to two variables, 
+# and then print them.
+name = None
+age = None
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+print(f"You are {name} and you are {age} years old.")
